97b18804d7b577e4f368ce94321c73b66ac9438b
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-02-26 20:18:32 -0800

    Update e2e.sh

--- test/e2e.sh
+++ test/e2e.sh
@@ -67,7 +67,7 @@ cat << EOF
 EOF
 
 echo memory profiling /v1.1/health/memory
-curl -u "admin:$EC_ADM_TKN" http://localhost:$PORT/v1.1/health/memory
+curl -u "admin:$EC_ADM_TKN" -sS http://localhost:$PORT/v1.1/health/memory
 
 cat << EOF
 
