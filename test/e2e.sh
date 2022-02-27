3f38e80497a0ed7fdd1f97e6291927778713fa56
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-02-27 15:14:37 -0800

    Update e2e.sh

--- test/e2e.sh
+++ test/e2e.sh
@@ -18,7 +18,7 @@ cat << EOF
 -------------------------------------
 EOF
 
-EC_SETTING=$(echo '{"my-test-id":{"ids":["my-aid-1","my-aid-2"],"trustedIssuerIds":["legacy-cf-uaa-url"]}}' | base64 -w0)      
+EC_SETTING=$(printf "{\"%s\":{\"ids\":[\"my-aid-1\",\"my-aid-2\"],\"trustedIssuerIds\":[\"legacy-cf-uaa-url\"]}}" "$EC_SVC_ID" | base64 -w0)      
 
 cat << EOF
 
