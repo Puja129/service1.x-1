9907ad915c62f4cb7ae03c03c42da344deb14859
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-02-27 23:09:17 -0800

    Update e2e.sh

--- test/e2e.sh
+++ test/e2e.sh
@@ -92,7 +92,7 @@ EOF
 x=1
 while [ $x -le 5 ]
 do
-  curl -u "admin:$EC_ADM_TKN" -sS -w "\n[$x] total time taken: %{time_total}s\n" --output /dev/null http://localhost:$PORT/v1.1/health/memory
+  curl -u "admin:$EC_ADM_TKN" -sS -w "\n[$x] total time taken: %{time_total}s\n" http://localhost:$PORT/v1.1/health/memory
   x=$(( $x + 1 ))
   sleep 1
 done
@@ -109,7 +109,7 @@ EOF
 x=1
 while [ $x -le 5 ]
 do
-  curl -u "admin:$EC_ADM_TKN" -sS -w "\n[$x] total time taken: %{time_total}s (test content discarded)\n" http://localhost:$PORT/v1.1/api/pubkey
+  curl -u "admin:$EC_ADM_TKN" -sS -w "\n[$x] total time taken: %{time_total}s (test content discarded)\n" --output /dev/null http://localhost:$PORT/v1.1/api/pubkey
   x=$(( $x + 1 ))
   sleep 1
 done
