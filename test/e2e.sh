885a33f57329eb7ef90b39c9b932c0d2b6df509d
A. Yasuda <70172309+ayasuda-ge@users.noreply.github.com>
2022-03-11 17:01:37 -0800

    Update e2e.sh

--- test/e2e.sh
+++ test/e2e.sh
@@ -57,14 +57,13 @@ docker run --name=svc \
 -e PORT=$PORT \
 -v $(pwd)/svcs:/root/svcs \
 -p $PORT:$PORT \
-ghcr.io/ec-release/svc:v1.1
-#-d \
-#ghcr.io/ec-release/svc:v1.1 &> /dev/null
+-d \
+ghcr.io/ec-release/svc:v1.1 &> /dev/null
 
 #curl "http://localhost:$PORT/v1.1/info/"
 
 #docker logs svc --tail 1000
-exit 0
+#exit 0
 
 cat << EOF
 
