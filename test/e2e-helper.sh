590a8b3857eb41f9093de4a21a11890b93105bdc
Puja129 <pujashar9@gmail.com>
2022-03-15 11:44:57 -0400

    Update e2e-helper.sh

--- test/e2e-helper.sh
+++ test/e2e-helper.sh
@@ -6,10 +6,10 @@ function fetchCognitoTkn() {
   echo my task
 
 }
-: 'my_token=$(fetchCognitoTkn "$COGNITO_URL" "$COGNITO_CID" "$COGNITO_CSC")
-jwtdec=$(echo "$my_token" | jq -R 'split(".") | .[0] | @base64d | fromjson')
-kid=$(echo "$jwtdec" | jq -r '.kid')
-echo kid: $kid'
+#my_token=$(fetchCognitoTkn "$COGNITO_URL" "$COGNITO_CID" "$COGNITO_CSC")
+#jwtdec=$(echo "$my_token" | jq -R 'split(".") | .[0] | @base64d | fromjson')
+#kid=$(echo "$jwtdec" | jq -r '.kid')
+#echo kid: $kid
 
 : 'cat << EOF
 
